748b40c8f173bb8e49e403eefadc50972aa97c5e
Sarah Donehower <sarahdonehower@gmail.com>
2018-10-15 15:20:25 -0700

    added data_setup directory to ignored files

--- data_setup/states.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-st_abbr_list = []
-with open('/Users/sarahdonehower/Documents/projects/weather_site/data_setup/state_abbr.csv', encoding='utf-8-sig') as f:
-    csv_reader = csv.reader(f, strict=False)
-    for row in csv_reader:
-        st_abbr_list.append((row[1], row[0]))
