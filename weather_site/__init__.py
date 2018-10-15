0296f9a4f41c68b9a2ca69adfa3d3c60c6435b0f
Sarah Donehower <sarahdonehower@gmail.com>
2018-10-15 15:24:39 -0700

    Deleted variables moved to config file in instance folder

--- weather_site/__init__.py
+++ weather_site/__init__.py
@@ -5,12 +5,10 @@ from flask_migrate import Migrate
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
-app.config['SECRET_KEY'] = 'secretkey'
 
 # --------------------------------- #
 #         Database Set-up           #
 # --------------------------------- #
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sarahd:avocado_good@localhost:5432/weather_site'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
