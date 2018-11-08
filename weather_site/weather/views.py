6fcf068b2ad542c0318d48f506d9f7496c6d735b
Sarah Donehower <sarahdonehower@gmail.com>
2018-11-07 18:14:04 -0800

    Added views for page and graph displays

--- weather_site/weather/views.py
+++ weather_site/weather/views.py
@@ -1,10 +1,10 @@
 from flask import render_template, Blueprint
 from flask import request, jsonify
-from bokeh.embed import components
-from weather_site.graphs.graphs_bar import bar_1
 from weather_site.weather.forms import FindLocation
 from weather_site.models import Locations
 from weather_site.data_pulls.current_conditions import get_current_conditions
+from weather_site import mongo
+from pymongo import MongoClient
 
 
 weather = Blueprint('weather', __name__)
@@ -62,13 +62,33 @@ def _get_conditions():
 @weather.route('/forecast')
 def forecast():
 
-    x = ["Jan", "Feb", "Mar", "Apr", "May", "June"]
-    y = [1.2, 2.5, 3.7, 4, 8, 3]
+    form = FindLocation(form_name='PickCity')
 
-    plot = bar_1(x, y)
+    all_states = mongo.db.forecast_5day.find({}, {'_id': 0, 'state': 1, 'state_full': 2})
+    form.state_full.choices = sorted(set([(n['state'], n['state_full']) for n in all_states]))
 
-    script, div = components(plot)
-    return render_template('forecast.html', script=script, div=div)
+    all_cities = mongo.db.forecast_5day.find({}, {'_id': 0, 'city': 1})
+    form.city.choices = sorted(set([(n['city'], n['city']) for n in all_cities]))
+
+    return render_template('forecast.html', form=form)
+
+
+@weather.route('/_get_forecast_cities')
+def _get_forecast_cities():
+    new_state = request.args.get('state', '01', type=str)
+    cities = sorted(set([(n['city'], n['city']) for n in mongo.db.forecast_5day.find({'state': new_state})]))
+
+    return jsonify(cities)
+
+
+@weather.route('/_get_forecast_data', methods=['GET', 'POST'])
+def _get_forecast_data():
+    new_city = request.args.get('city', '01', type=str)
+    forecast = mongo.db.forecast_5day.find_one({'city': new_city})
+    if '_id' in forecast.keys():
+        forecast.pop('_id')
+
+    return jsonify(forecast)
 
 # --------------------------- BUOY REPORTING -------------------------------- #
 
